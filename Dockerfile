10bd1d4b836bb4f829590e0c63e10284cc14e608
Tuan Nguyen <tuannvm@tuannvm.com>
2017-06-21 18:31:00 +0800

    use python sdk

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM alpine:3.6
+FROM docker:17.06.0-ce-dind
 RUN apk --update add py-pip bash curl tar docker && \
    pip install awscli
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 WORKDIR /srv/
 RUN cd $WORKDIR
 COPY entrypoint.py .
+# COPY Dockerfile-test .
 ENTRYPOINT ["/srv/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
+import docker
+import time
 
-# print env
-print os.environ
+os.system("dockerd &")
+
+time.sleep(3)  # delays for 5 seconds
+
+client = docker.DockerClient(version="auto")
+
+# client.images.build(path="./", tag="tuannvm/latest", cache_from=["alpine"], dockerfile="Dockerfile-test", timeout=10)
