9dd6815d908f86b950a84a653bf6726ac510c532
Tuan Nguyen <tuannvm@tuannvm.com>
2017-06-22 00:13:52 +0800

    sample testhard code

--- entrypoint.py
+++ entrypoint.py
@@ -12,4 +12,4 @@ time.sleep(3)  # delays for 5 seconds
 
 client = docker.DockerClient(version="auto")
 
-client.images.build(path="hembuildbase/", tag="tuannvm/latest", cache_from=["alpine"], dockerfile="Dockerfile", timeout=10)
+client.images.build(path="./hembuildbase/", tag="tuannvm/latest", cache_from=["alpine"], dockerfile="Dockerfile", timeout=10)
