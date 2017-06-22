9e2cfb83e14ba3291189c146686f07ce0b822197
Tuan Nguyen <tuannvm@tuannvm.com>
2017-06-22 11:55:33 +0800

    test build

--- entrypoint.py
+++ entrypoint.py
@@ -5,14 +5,31 @@
 import os
 import docker
 import time
+import commands
+
+REPOSITORY = os.getenv("PLUGIN_REGISTRY") or "quay.io"
+ORGANIZATION = os.getenv("PLUGIN_ORGANIZATION") or "honestbee"
+base_path = os.getenv("DRONE_REPO_LINK").split("//")[1] + "/"
+last_commit_id = os.getenv("DRONE_PREV_COMMIT_SHA") or "3b2b483ec8e81ecd88db428111cc89474b9cc37c"
+cmd = "cd " + base_path + " && git diff --name-status " + last_commit_id + " HEAD | awk '{print $2}'"
 
 os.system("dockerd &")
-
 time.sleep(3)  # delays for 5 seconds
 
 client = docker.DockerClient(version="auto")
-last_commit = os.getenv("DRONE_PREV_COMMIT_SHA") or "276b8fffee67ddadb1c616caae3fb6a967a7b462"
-cmd = "git diff --name-status " + last_commit + " HEAD | grep Dockerfile"
-print os.system(cmd)
+# cmd = "cd /Users/tuannvm/Projects/honestbee/base-images && git diff --name-status " + last_commit_id + \ " HEAD | awk '{print $2}'"
 
-client.images.build(path="helmbuildbase/", tag="tuannvm/latest", cache_from=["alpine"], dockerfile="Dockerfile", timeout=10)
+status, output = commands.getstatusoutput(cmd)
+changed_objects = output.split("\n")
+
+for ob in changed_objects:
+    if ob != ".drone.yml" and ob != ".drone.yml.sig":
+        folder_path = os.path.dirname(ob)
+        if os.path.isfile(folder_path + "/Dockerfile"):
+            client.images.build(path=folder_path,
+                                tag=REPOSITORY + "/" + ORGANIZATION + "/" + folder_path + ":latest",
+                                rm=True,
+                                cache_from=["alpine"],
+                                timeout=120)
+        else:
+            print "No changes. Skipping build..."
