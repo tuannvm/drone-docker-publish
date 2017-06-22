58a569cf58f8c22958afeb39fe56ede89f1860e1
Tuan Nguyen <tuannvm@tuannvm.com>
2017-06-22 14:03:14 +0800

    test build with docker credentials

--- entrypoint.py
+++ entrypoint.py
@@ -7,9 +7,9 @@ import docker
 import time
 import commands
 
-# USERNAME = os.getenv("DOCKER_USERNAME") or "tuannvm+quaybot"
+# USERNAME = os.getenv("DOCKER_USERNAME")
 USERNAME = os.getenv("DOCKER_USERNAME")
-# PASSWORD = os.getenv("DOCKER_PASSWORD") or "V9WSBL9LWNGTTQBHE4J7C37PWC07JJAM65W2EMK1VWLK3I5FBEGQ7EWWHV2UPXD7"
+# PASSWORD = os.getenv("DOCKER_PASSWORD")
 PASSWORD = os.getenv("DOCKER_PASSWORD")
 REPOSITORY = os.getenv("DOCKER_REGISTRY")
 ORGANIZATION = os.getenv("PLUGIN_ORGANIZATION")
@@ -18,6 +18,7 @@ base_path = os.getenv("DRONE_REPO_LINK").split("//")[1] + "/"
 last_commit_id = "3b2b483ec8e81ecd88db428111cc89474b9cc37c"
 # last_commit_id = os.getenv("DRONE_PREV_COMMIT_SHA")
 cmd = "git diff --name-status " + last_commit_id + " HEAD | awk '{print $2}'"
+duplicate_folder_path = ""
 
 os.system("dockerd &")
 time.sleep(3)  # delays for 5 seconds
@@ -38,14 +39,19 @@ print changed_objects
 for ob in changed_objects:
     if ob != ".drone.yml" and ob != ".drone.yml.sig":
         folder_path = os.path.dirname(ob)
+        if folder_path == duplicate_folder_path:
+            break
         client.login(username=USERNAME, password=PASSWORD, registry=REPOSITORY)
         if os.path.isfile(folder_path + "/Dockerfile"):
+            print "building " + folder_path + " image..."
             client.images.build(path=folder_path,
                                 tag=REPOSITORY + "/" + ORGANIZATION + "/" + folder_path + ":latest",
                                 rm=True,
                                 cache_from=["alpine"],
                                 timeout=120)
 
+            print "pushing " + folder_path + " to " + ORGANIZATION + "..."
             client.images.push(repository=REPOSITORY + "/" + ORGANIZATION + "/" + folder_path, tag="latest")
+            duplicate_folder_path = folder_path
         else:
             print "No changes. Skipping build..."
