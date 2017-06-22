a67b323e6835d2a2d67136c4d9519ff3d587f53a
Tuan Nguyen <tuannvm@tuannvm.com>
2017-06-22 10:18:33 +0800

    fix indent

--- entrypoint.py
+++ entrypoint.py
@@ -6,9 +6,9 @@ import os
 import docker
 import time
 
- os.system("dockerd &")
+os.system("dockerd &")
 
- time.sleep(3)  # delays for 5 seconds
+time.sleep(3)  # delays for 5 seconds
 
 client = docker.DockerClient(version="auto")
 last_commit = os.getenv("DRONE_PREV_COMMIT_SHA") or "276b8fffee67ddadb1c616caae3fb6a967a7b462"
